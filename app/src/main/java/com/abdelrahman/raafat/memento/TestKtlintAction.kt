0e67df183c3a377e14687fcd562ae38b1cfac75e
Abdelrahman Rafaat <abdelrahman.raafaat@gmail.com>
2026-01-08 12:10:48 +0200

    - Test code against Ktlint Rules

---
+++ app/src/main/java/com/abdelrahman/raafat/memento/TestKtlintAction.kt
@@ -0,0 +1,10 @@
+package com.abdelrahman.raafat.memento
+//This branch violets the Ktlitn standrad and must fail the Ktlint Action on PR
+class TestKtlintAction                 {
+
+    fun TEST(               ){
+
+    }
+
+
+}
