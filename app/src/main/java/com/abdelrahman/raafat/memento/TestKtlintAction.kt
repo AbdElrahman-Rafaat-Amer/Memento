f18679aa09ae385b932b29464f27a0be119265c2
Abdelrahman Rafaat <abdelrahman.raafaat@gmail.com>
2026-01-08 20:25:55 +0200

    - Made function follow Ktlint rules

--- app/src/main/java/com/abdelrahman/raafat/memento/TestKtlintAction.kt
+++ app/src/main/java/com/abdelrahman/raafat/memento/TestKtlintAction.kt
@@ -2,6 +2,6 @@ package com.abdelrahman.raafat.memento
 
 // This branch violets the Ktlitn standrad and must fail the Ktlint Action on PR
 class TestKtlintAction {
-    fun TEST() {
+    fun test() {
     }
 }
